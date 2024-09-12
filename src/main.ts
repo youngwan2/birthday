7b4fb99a60dc152831f3bea2b6a78313cc022b9a
Kim Young Wan <qodna25@gmail.com>
2024-09-12 13:17:56 +0900

    모바일 꺠짐 수정

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,13 @@ const content = document.querySelector('.content') as HTMLDivElement
 
 button?.addEventListener('click', () => {
 
-   new Typed('.text', {
+  new Typed('.text', {
     strings:
-      ["형 생일 축하함.", "시간이 정말 어떻게 가는지 모를 정도로 빠르네, 벌써 형 생일이라니.",
-        " 올해 얼마 안 남았지만, 현재 계획하고 있는 것들 하나씩 이루어지면 좋겠다.",
-        " 늘 생각하지만, 건강 챙기면서 일하고, 행복한 생일날이 되길 바람.",
-        " 다시 한 번 생일 축하해~.",
+      [" 생일 축하함.",
+      " 시간 정말 빠른 듯.",
+        " 남은 올해 목표하는거 하나씩 이루어지면 좋겠음.",
+        " 건강 챙기며 일하고,",
+        " 재밌는 날 되길 바람.",
         " Happy birthday 🎉"
       ],
     startDelay: 1000,
